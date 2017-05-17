67bcf72d23af2d7914b5e0c2aed5e063da506e5d
robbie <fernandez.robert.97@gmail.com>
2017-05-17 19:02:19 +0800

    Profile controller now checks user exists when adding a new profile

--- server/controllers/profile.js
+++ server/controllers/profile.js
@@ -1,5 +1,6 @@
 require('../models/db');
 var mongoose = require('mongoose');
+var User = mongoose.model('User');
 var Profile = mongoose.model('Profile');
 
 module.exports.getProfile = function(req, res) {
@@ -26,30 +27,52 @@ module.exports.getProfile = function(req, res) {
 }
 
 module.exports.addProfile = function(req, res) {
-	var newProfile = new Profile ({
-			username: req.body.username,
-			firstname:  req.body.firstname,
-			middlename: req.body.middlename,
-			lastname:   req.body.lastname,
-			bio: req.body.bio,
-			//TODO: Loop over interests
-			interests:   req.body.interests,
-			age:        req.body.age,
-			gender:     req.body.gender
-	});
-	console.log(newProfile);
-	newProfile.save(function(err, data){
-		if(err) {
-			console.log(err);
-			res.status(500);
-			res.render('error', {
-				message:err.message,
-				error:err
-			});
-		} else {
-			console.log(data, 'saved');
-			res.render('profile', {'profile' : newProfile});
+	//Check if username exists
+	User.find({username: req.body.username}).exec(
+		function(err, found) {
+			if (err) {
+				console.log(err);
+				res.render('error', {
+					message:err.message,
+					error:err
+				});
+			} else {
+				if (found.length > 0) {
+					addProfile();
+				} else {
+					res.setHeader('Content-Type', 'application/json');
+    				res.send(JSON.stringify({ message : "User doesn't exist"}));
+				}
+			}
 		}
-	})
+	);
 
+	var addProfile = function() {
+		//Construct profile model
+		var newProfile = new Profile ({
+				username: req.body.username,
+				firstname:  req.body.firstname,
+				middlename: req.body.middlename,
+				lastname:   req.body.lastname,
+				bio: req.body.bio,
+				//TODO: Loop over interests
+				interests:   req.body.interests,
+				age:        req.body.age,
+				gender:     req.body.gender
+		});
+		console.log(newProfile);
+		newProfile.save(function(err, data){
+			if(err) {
+				console.log(err);
+				res.status(500);
+				res.render('error', {
+					message:err.message,
+					error:err
+				});
+			} else {
+				console.log(data, 'saved');
+				res.render('profile', {'profile' : newProfile});
+			}
+		});
+	}
 }
