4ab4ab39e63a472e893de770800ceab9f6a0e845
robbie <fernandez.robert.97@gmail.com>
2017-05-17 18:29:15 +0800

    Cleaned up email & username checking callbacks in user controller

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -23,8 +23,8 @@ function index(req, res) {
 }
 
 module.exports.addUser = function(req, res) {
-	//First see if username is taken
-	User.find({username:req.body.username}).exec(
+	//Check username. Call email callback if valid
+	User.find({username: req.body.username}).exec(
 		function(err, usersFound) {
 			if (err) {
 				res.render('error', {
@@ -36,42 +36,52 @@ module.exports.addUser = function(req, res) {
 					res.setHeader('Content-Type', 'application/json');
     				res.send(JSON.stringify({ message : "Username taken"}));
 				} else {
-					//Username not taken, now check email
-					User.find({email:req.body.email}).exec(
-						function(err, usersFound){
-							if (err) {
-								res.render('error', {
-									message:err.message,
-									error: err
-								});
-							} else {
-								if (usersFound.length > 0) {
-									res.setHeader('Content-Type', 'application/json');
-    								res.send(JSON.stringify({ message : "Email taken"}));
-								} else {
-									//Username and email are available, now add users
-									var newUser = new User ({
-											username:      req.body.username,
-											password:      req.body.password,
-											email:         req.body.email,
-									});
-									newUser.save(function(err, data){
-										if(err) {
-											console.log(err);
-											res.status(500);
-											res.render('error', {
-												message:err.message,
-												error:err
-											});
-										} else {
-											console.log(data, 'saved');
-											index(req, res);
-										}
-									});		
-								}		
-							}
-						});
-					}
+					checkEmail();
 				}
-			});
+			}
 		}
+	);
+
+	//Callback for checking email. Calls user callback if valid
+	var checkEmail = function() {
+		User.find({email: req.body.email}).exec(
+			function(err, usersFound){
+				if (err){
+					res.render('error', {
+						message:err.message,
+						error: err
+					});
+				} else {
+					if (usersFound.length > 0) {
+						res.setHeader('Content-Type', 'application/json');
+    					res.send(JSON.stringify({ message : "Email taken"}));
+    				} else {
+    					addUser();
+    				}
+				}
+			}
+		);
+	}
+	
+	//Callback for adding the user
+	var addUser = function() {
+				var newUser = new User ({
+				username:      req.body.username,
+				password:      req.body.password,
+				email:         req.body.email,
+		});
+		newUser.save(function(err, data){
+			if(err) {
+				console.log(err);
+				res.status(500);
+				res.render('error', {
+					message:err.message,
+					error:err
+				});
+			} else {
+				console.log(data, 'saved');
+				index(req, res);
+			}
+		});
+	}
+}
