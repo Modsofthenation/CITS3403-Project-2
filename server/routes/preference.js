b6a35dfd132f70fc13ee88ea9cb09a48f5ee86e6
robbie <fernandez.robert.97@gmail.com>
2017-05-26 16:45:17 +0800

    Deleted preference route/model

--- server/routes/preference.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var ctrlPreference = require('../controllers/preference.js');
-
-router.post('/', function(req, res, next) {
-	ctrlPreference.addPreference(req, res);
-});
-
-module.exports = router;
