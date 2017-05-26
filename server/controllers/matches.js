40362d8114b97ab4f338d4eba5c778128643c93a
robbie <fernandez.robert.97@gmail.com>
2017-05-26 21:56:23 +0800

    Fixed shared interest count

--- server/controllers/matches.js
+++ server/controllers/matches.js
@@ -18,21 +18,25 @@ module.exports.getMatches = function(req, res) {
 	Profile.find(find).exec(function(err, profiles) {
 		//Find user's profile then filter out the matches that are too distant
 		Profile.findOne({'username': req.user.username}).exec( function(err, userProfile) {
-			filterDistance(req.body.distance, userProfile, profiles);
-			sortByScore(userProfile, profiles);
+			profiles = filterDistance(req.body.distance, userProfile, profiles);
+			profiles = sortByScore(userProfile, profiles);
 			res.render('matches', {matches: profiles, user: req.user});
 		});
 	});
 }
 
-function filterDistance(distance, userProfile, matchProfiles) {
+function filterDistance(maxDistance, userProfile, matchProfiles) {
 	var lat1 = userProfile.lattitude;
 	var lng1 = userProfile.longitude;
 
 	for(i=(matchProfiles.length - 1); i >=0; i--) {
-		if (getDistance(lat1, lng1, matchProfiles[i].lattitude, matchProfiles[i].longitude) > distance)
-				matchProfiles.splice(i, 1);
+		var distance  = getDistance(lat1, lng1, matchProfiles[i].lattitude, matchProfiles[i].longitude);
+		if ( distance > maxDistance)
+			matchProfiles.splice(i, 1);
+		else
+			matchProfiles[i].distance = Math.round(distance);
 	}
+	return matchProfiles;
 }
 
 function getDistance(lat1, lon1, lat2, lon2) {
@@ -56,24 +60,35 @@ function degreesToRadians(degrees) {
 };
 
 function sortByScore(user, profiles) {
+	console.log("Profiles to score: " + profiles);
 	//calculate all scores.
-	for (i=0; i<profiles.length; i++) {
-		profiles[i].score = calculateScore(user, profiles[i]);
+	for(i=(profiles.length - 1); i > -1; i--) {
+		var score = calculateScore(user, profiles[i]);
+		if (score < 0 )
+			profiles.splice(i, 1);
+		else
+			profiles[i].score = score;
 	}
+
 	//sort by scores
 	profiles.sort(compareScoredProfiles);
+	return profiles;
 }
 
 function calculateScore(user, profile) {
-	var score = 0;
-	for (i=0; i<user.interests.length; i++) {
-		if (profile.interests.indexOf(user.interests[i]) != -1)
-			score++;
-	}
-	return score;
+	var common = intersect(user.interests, profile.interests);
+	return common.length;
 }
 
 //Assumes profiles already contain scores
 function compareScoredProfiles(profile1, profile2) {
-	return profile2.score - profile1.score;
+	return (profile2.score - profile1.score);
+}
+
+function intersect(a, b) {
+    var t;
+    if (b.length > a.length) t = b, b = a, a = t; // indexOf to loop over shorter
+    return a.filter(function (e) {
+        return b.indexOf(e) > -1;
+    });
 }
